817b94118d50b345eed01fb6295f57e2a4b82dfd
TGsum <125791508+TGsum@users.noreply.github.com>
2025-06-05 16:29:24 +0800

    Add files via upload
    使用了node.js对前后端进行桥接，并自创HTML文件通过了web到后端的测试、、、注意（web端口请使用json请求）

--- LibraryManagementSystem/src/JDBCPoolConnectionTest.java
+++ LibraryManagementSystem/src/JDBCPoolConnectionTest.java
@@ -5,10 +5,17 @@ import java.sql.SQLException;
 
 
 public class JDBCPoolConnectionTest {
-//    public static void main(String[] args) throws SQLException {
-//        Connection connection = JDBCUtilsByDruid.getConnection();
-//        System.out.println(connection);
+    public static void main(String[] args) throws SQLException {
+        Connection connection = JDBCUtilsByDruid.getConnection();
+        System.out.println(connection);
+    }
+
+//    public static void main(String[] args) {
+//        System.out.println("你好，世界！");
+//        System.out.println("当前编码：" + System.getProperty("file.encoding"));
+//
 //    }
+
 
 
 }
